64aea748262b32c1b35d65c2f56794f53f77fc3a
GXH-debug <2874903098@qq.com>
2022-07-26 17:24:53 +0800

    添加项目文件。

---
+++ speaker.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Speaker
+{
+public:
+	string m_Name;//ĞÕÃû
+	double m_Score[2];//·ÖÊı
+};
---
+++ speechManager.h
@@ -0,0 +1,81 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <map>
+#include "speaker.h"
+#include <algorithm>
+#include <deque>
+#include <numeric>
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+//设计演讲比赛管理类
+class SpeechManager
+{
+public:
+	
+	//构造函数
+	SpeechManager();
+
+	//展示菜单
+	void show_Menu();
+
+	//退出系统
+	void exitSystem();
+
+	//析构函数
+	~SpeechManager();
+
+	//初始化容器和属性
+	void initSpeech();
+
+	//创建12名选手
+	void createSpeaker();
+
+	//开始比赛 - 比赛整个流程控制函数
+	void startSpeech();
+
+	//抽签
+	void speechDraw();
+
+	//比赛
+	void speechContest();
+
+	//显示比赛结果
+	void showScore();
+
+	//保存记录
+	void saveRecord();
+
+	//读取记录
+	void loadRecord();
+
+	//文件为空的标志
+	bool fileIsEmpty;
+
+	//往届记录
+	map<int, vector<string>> m_Record;
+
+	//显示往届得分
+	void showRecord();
+
+	//清空记录
+	void clearRecord();
+	
+	//成员属性
+	//保存第一轮选手编号容器
+	vector<int>v1;
+
+	//第一轮晋级选手编号容器
+	vector<int>v2;
+
+	//胜出前三名选手编号容器
+	vector<int>vVictory;
+	
+	//存放编号以及对应具体选手
+	map<int, Speaker>m_Speaker;
+
+	//存放比赛轮数
+	int m_Index;
+};
